345458b972a27ad2e24c1bc8a75bc8dca65b338b
Herbert Parasio <91493748+Herbertph@users.noreply.github.com>
2024-06-06 14:40:21 -0300

    Add files via upload

---
+++ Capitulo 1/main.go
@@ -0,0 +1,40 @@
+package main
+
+/*Exercise1
+func main() {
+	x := 42
+	y := "James Bond"
+	z := true
+
+	fmt.Println(x, y, z)
+
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+}*/
+
+/*Exercise2
+var x int
+var y string
+var z bool
+
+func main() {
+
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+
+}*/
+
+/*Exercise3
+var x int = 42;
+var y string = "James Bond"
+var z bool = true
+
+func main() {
+
+
+s := fmt.Sprintf("%v %v %v", x, y, z)
+fmt.Println(s)
+
+}*/
